25c4960f5954c446e08f075a602dbfd5a3148780
kateliaskov <58365756+KateLis84@users.noreply.github.com>
2025-08-07 22:44:10 +0300

    finish the task

--- cypress/e2e/signIn.cy.js
+++ cypress/e2e/signIn.cy.js
@@ -1,7 +1,29 @@
 /// <reference types="cypress" />
+import { faker } from '@faker-js/faker';
 
 describe('Sign In page', () => {
-  it('should provide an ability to log in', () => {
+  beforeEach(() => {
+    cy.visit('');
+  });
 
+  const username = faker.internet.userName().replace('.', '');
+  const email = faker.internet.email();
+  const password = faker.internet.password();
+
+  it('should register and login then', () => {
+    cy.request('POST', '/api/users/', {
+      user: {
+        username,
+        email,
+        password
+      }
+    }).its('status').should('eq', 200);
+
+    cy.get(':nth-child(2) > .nav-link').click();
+
+    cy.get('input[placeholder="Email"]').type(email);
+    cy.get('input[placeholder="Password"]').type(password);
+
+    cy.get('.btn-primary').click();
   });
 });
